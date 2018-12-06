e51d55238b08426f93fc083ccab16b8d7f171d76
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2018-12-06 19:46:46 +0100

    aaaqaaaa

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function losuj(min, max){
-return Math.floor(Math.random() * (max - min + 1) + min);
+const buttonsKey = document.getElementsByClassName('key');
+let lastKey;
 
-}
+
+function getRandomKey() {
+    index = Math.floor(Math.random() * buttonsKey.length);
+    console.log(index)
+    const key = buttonsKey[index];
+    if (key === lastKey) {
+       console.log('The same key!!!')
+       return getRandomKey()
+    }
+    lastKey = key;
+    return key;
+};
+
+function losuj(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+console.log(getRandomKey())
