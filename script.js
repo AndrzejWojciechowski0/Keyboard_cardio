02640f34df66bd6161fd7d6b875ea88810594c58
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2018-12-13 18:21:44 +0100

    sajvbjkabvws

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const buttonsKey = document.getElementsByClassName('key');
 let lastKey;
 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 function getRandomKey() {
     index = Math.floor(Math.random() * buttonsKey.length);
-    console.log(index)
     const key = buttonsKey[index];
     if (key === lastKey) {
-       console.log('The same key!!!')
-       return getRandomKey()
+        console.log('The same key!!!')
+        return getRandomKey()
     }
     lastKey = key;
     return key;
 };
 
-function losuj(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
+function peep(){
 
-console.log(getRandomKey())
+    
+}
